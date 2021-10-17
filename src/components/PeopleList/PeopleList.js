d56d3a7c9437d45dd5cb2eeccbb925ad1d01ca5a
Topmewan <anon8733@gmail.com>
2021-10-17 20:06:40 +0300

    style PeopleList

--- src/components/PeopleList/PeopleList.js
+++ src/components/PeopleList/PeopleList.js
@@ -2,15 +2,15 @@ import styles from './PeopleList.module.css';
 
 const PeopleList = ({character}) => {
     return (
-        <>
-            <ul>
+            <ul className={styles.list__container}>
                 {character.map(({name,img,char_id}) =>
-                    <li key={char_id}>
-                        <img src={img} alt={name}/>
-                        <p>{name}</p>
+                    <li key={char_id} className={styles.list__item}>
+                        <a href="#">
+                            <img className={styles.person__photo} src={img} alt={name}/>
+                            <p>{name}</p>
+                        </a>
                     </li>)}
             </ul>
-        </>
     );
 
 }
