5e9f9f82aea9b270e983b91882a90164229d310e
Topmewan <anon8733@gmail.com>
2021-10-17 14:58:32 +0300

    getApi func + conenct to api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import cn from 'classnames';
 import styles from './App.module.css';
+import {getApi} from "./utils/api";
+
 console.log(styles);
+getApi();
+
 
 function App() {
+
+
+
   return (
-    <div className={cn(styles.header,styles.text)}>
-      gsfsgsg
+    <div className={cn(styles.header)}>
+
 
 
     </div>
---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+const API_ROOT = 'https://www.breakingbadapi.com/api/';
+const API_SECOND = 'characters';
+
+export const getApi = async (url) => {
+    try {
+        const res = await fetch(url);
+        if(!res.ok){
+            console.error('Could not fetch',res.status);
+            return false;
+        }
+        return await res.json();
+    } catch (e) {
+        console.error(e.message);
+        return false;
+    }
+}
+
+
+
+(async () => {
+    const body = await getApi(API_ROOT + API_SECOND);
+    console.log(body);
+})();
