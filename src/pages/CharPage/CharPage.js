a7758f6257fee051749e1227f74c703c7efc0ed3
Topmewan <anon8733@gmail.com>
2021-10-30 17:42:27 +0300

    rendering charpage v1

--- src/pages/CharPage/CharPage.js
+++ src/pages/CharPage/CharPage.js
@@ -1,11 +1,15 @@
 import styles from './CharPage.module.css';
 import {getApi} from "../../utils/getApi";
 import PropTypes from "prop-types";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {API_CHAR} from "../../constants/api";
 import {withErrorApi} from "../../HOC/withErrorApi";
 
 const CharPage = ({match, setErrorApi}) => {
+
+    const [charInfo,setCharInfo] = useState(null);
+    const [charName,setCharName] = useState(null);
+
 
     useEffect(()=>{
         (async () => {
@@ -13,6 +17,17 @@ const CharPage = ({match, setErrorApi}) => {
             const res = await getApi(`${API_CHAR}/${id}/`);
 
             if(res){
+                setCharInfo([
+                    {title:'Height',data: res.height},
+                    {title:'Mass',data: res.mass},
+                    {title:'Hair Color',data: res.hair_color},
+                    {title:'Skin Color',data: res.skin_color},
+                    {title:'Eye Color',data: res.eye_color},
+                    {title:'Birth Year',data: res.birth_year},
+                    {title:'Gender',data: res.gender},
+                ]);
+                setCharName(res.name);
+
                 setErrorApi(false);
             } else {
                 setErrorApi(true);
@@ -23,9 +38,20 @@ const CharPage = ({match, setErrorApi}) => {
     },[]);
 
     return (
-        <div>
+        <>
+            <h1>{charName}</h1>
+            {charInfo && (
+                <ul>
+                    {charInfo.map(({title,data})=> (
+                        data && <li key={title}>
+                            <span>{title}:{data}</span>
+                        </li>
+                        ))}
+                </ul>
+            )}
 
-        </div>
+
+        </>
     );
 
 }
